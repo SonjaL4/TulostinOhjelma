c8832657e7c9e5e05b0155ad32265b0f7fbce357
SonjaL4 <143099782+SonjaL4@users.noreply.github.com>
2023-10-09 21:25:08 +0300

    Loin kaksi int-muuttujaa ja Stringin ja annoin niille arvot . Lisäksi kaksi tulostetta int luku1 ja String muuttuja.

--- src/tulostinOhjelma.java
+++ src/tulostinOhjelma.java
@@ -2,9 +2,17 @@
 public class tulostinOhjelma {
 
 	public static void main(String[] args) {
+		String muuttuja= "Sonja";
+		int luku1= 4 ;
+		int luku2 = 5; 
+		
+		
 		System.out.println("Hei olen Tulostin-ohjelma");
-		System.out.println("Ohjelman tekijä:");
-
+		System.out.print("Ohjelman tekijä:");
+		System.out.println(muuttuja);
+		System.out.println(luku1);
+		
+		
 	}
 
 }
