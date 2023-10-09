8830bba57a11db096958378234455d81c9d6a276
SonjaL4 <143099782+SonjaL4@users.noreply.github.com>
2023-10-09 21:47:18 +0300

    Kolme int:iä summa, erotus ja jako. Niille myös tulosteet.

--- src/tulostinOhjelma.java
+++ src/tulostinOhjelma.java
@@ -6,14 +6,21 @@ public class tulostinOhjelma {
 		int luku1= 4 ;
 		int luku2 = 5; 
 		int tulo =luku1*luku2;
-		
+		int erotus = luku1 - luku2 ;
+		int summa = luku1 + luku2;
+		int jako = luku1 / luku2;
 		
 		System.out.println("Hei olen Tulostin-ohjelma");
 		System.out.print("Ohjelman tekijä:");
 		System.out.println(muuttuja);
+		
 		System.out.println("Luku1-muuttujan arvo on "+luku1);
 		System.out.println("Luku1-muuttujan arvo on "+luku2);
+		
 		System.out.println(luku1+" * "+luku2+" = "+ tulo);
+		System.out.println(luku1+" - "+luku2+" = "+ erotus);
+		System.out.println(luku1+" + "+luku2+" = "+ summa);
+		System.out.println(luku1+" / "+luku2+" = "+ jako);
 		
 	}
 
