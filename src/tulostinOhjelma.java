56f272aff1183d4a518d6087fb63f379fb5521c8
SonjaL4 <143099782+SonjaL4@users.noreply.github.com>
2023-10-09 21:40:18 +0300

    loin muuttuja tulo ja kertolasku tulosteen.

--- src/tulostinOhjelma.java
+++ src/tulostinOhjelma.java
@@ -5,6 +5,7 @@ public class tulostinOhjelma {
 		String muuttuja= "Sonja";
 		int luku1= 4 ;
 		int luku2 = 5; 
+		int tulo =luku1*luku2;
 		
 		
 		System.out.println("Hei olen Tulostin-ohjelma");
@@ -12,7 +13,7 @@ public class tulostinOhjelma {
 		System.out.println(muuttuja);
 		System.out.println("Luku1-muuttujan arvo on "+luku1);
 		System.out.println("Luku1-muuttujan arvo on "+luku2);
-		
+		System.out.println(luku1+" * "+luku2+" = "+ tulo);
 		
 	}
 
